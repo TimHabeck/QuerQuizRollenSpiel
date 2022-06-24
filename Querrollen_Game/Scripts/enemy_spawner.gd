6f1d96d45aacf3cf13e5a665f81422dfae6268ec
Tim Habeck <th135@hdm-stuttgart.com>
2022-06-24 07:44:53 +0200

    Gegner direkt eingefügt

--- Querrollen_Game/Scripts/enemy_spawner.gd
+++ Querrollen_Game/Scripts/enemy_spawner.gd
@@ -3,20 +3,21 @@ extends Node
 var enemy_scene = preload("res://Szenes/Enemy.tscn")
 # Called when the node enters the scene tree for the first time.
 onready var spawn_points = [
-	{"x":-2817,"y":32},
-	{"x":1052,"y":32},
-	{"x":8179,"y":32},
-	{"x":-2953,"y":544},
-	{"x":3787,"y":544},
-	{"x":9420,"y":544},
-	{"x":5569,"y":1100},
+	{"x":-2817,"y":32+15},
+	{"x":1052,"y":32+15},
+	{"x":8179,"y":32+15},
+	{"x":-2953,"y":544+15},
+	{"x":3787,"y":544+15},
+	{"x":9420,"y":544+15},
+	{"x":5569,"y":1100+15},
 	]
 	
 func _ready():
-	for spawn in spawn_points:
+	"""for spawn in spawn_points:
 		var enemy = enemy_scene.instance()
 		enemy.position.x = spawn.x
 		enemy.position.y = spawn.y
 		add_child(enemy)
+		add_child(enemy)"""
 
 
